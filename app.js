3435e982e5f29cf52458c4d0f56c72075f3d30de
kenECV <117695216+kenECV@users.noreply.github.com>
2022-11-10 17:21:19 +0800

    partial commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ Vue.component('product',{
                     </a>
                 </div>
             </div>
-                <div class="product">
+            <div class="product">
                 <div class="product-details">
                     <h1>{{title}} <sup v-show="onSale">On Sale</sup></h1>
                     <p v-show="inStock"> in Stock</p>
@@ -44,11 +44,20 @@ Vue.component('product',{
                     <label v-for="(variantSize, sizeIndex) in Sizes"> {{ variantSize }}<input type="checkbox" :name="variantSize"
                             :value="variantSize"> </label>
                     <button @click="addToCart" :disabled="!inStock" class="btn btnBlue" :class="{disabledButton: !inStock }">Add to cart</button>
-            
+
+                    <p v-if="!reviews.length">There are no reviews yet </p>
+                    <ul>
+                        <li v-for="review in reviews">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                        </li>
+                    </ul>   
+                    <product-review @review-submitted="addReview"></product-review> <!-- calling addReview methods and passing the data from emit function-->
+                    <product-question ></product-question>
                 </div>
             </div>
-        </section>
-                           `,
+        </section>`,
     data() {
         return   {
         brand: 'Vue',
@@ -73,17 +82,19 @@ Vue.component('product',{
                 variantQuantity: 0
             },
           ],
-        //   cart:0,
+        reviews: [],
+        question: []
         }
     },
     methods:{
         addToCart() {
-            this.$emit('add-to-cart')
-            //  this.cart += 1
-            console.log('CLICKED')
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId )
         },
         updateProduct(index) {  
             this.selectedVariant = index
+        },      
+        addReview(productReview){ 
+            this.reviews.push( productReview ) // push the data to review attribute
         }
     },
     computed:{
@@ -110,15 +121,87 @@ Vue.component('product',{
 
 })
 
+Vue.component('product-review',{
+    template:`
+       <form class="review-form" @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+            <b>Please correct the following error(s): </b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+      </p>
+      
+      <p>
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+      
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+          
+      <p>
+        <input type="submit" value="Submit">  
+      </p>    
+    
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && history.review && this.rating){ // Check if the variables are empty
+                let productReview = { // adding all data to e produtReview variable
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview ) // Emiting he event triggered notice along with the productReview variable data to review-submitted
+                this.name = null   // Emptying the data
+                this.review = null // Emptying the data
+                this.rating = null // Emptying the data
+            }else{
+                this.errors = []   // Emptying the data
+                if( !this.name) this.errors.push("Name required.")     // Add error message
+                if( !this.review) this.errors.push("review required.") // Add error message
+                if( !this.rating) this.errors.push("rating required.") 
+            }
+            
+
+        }
+         }   
+})
+
+
+
+
 var app = new Vue({
     el: '#app',
     data:{
         premium: true,
-        cart: 0
+        cart: []
     },
-    method: {
-        updateCart(){
-            this.cart += 1
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
         }
     }
 })
